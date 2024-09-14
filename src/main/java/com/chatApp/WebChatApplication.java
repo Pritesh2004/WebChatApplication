da7c7f9010cf2ad28e210acee64eb1ba6719aeb1
Pritesh2004 <pmpatilpritesh123@gmail.com>
2024-09-14 21:51:52 +0530

    Updates

--- src/main/java/com/chatApp/WebChatApplication.java
+++ src/main/java/com/chatApp/WebChatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoChatAppApplication {
+public class WebChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoChatAppApplication.class, args);
+		SpringApplication.run(WebChatApplication.class, args);
 	}
 
 }
